aaf771bcf2d10e6024e4272df62642a489c87cac
KLC81 <timlook81@gmail.com>
2026-02-18 23:26:53 +0800

    Create relay control header file

---
+++ Drivers/Relay/relay.h
@@ -0,0 +1,15 @@
+// relay.h
+
+#ifndef RELAY_H
+#define RELAY_H
+
+// Function to initialize relay control
+void initRelay();
+
+// Function to turn relay on
+void turnRelayOn();
+
+// Function to turn relay off
+void turnRelayOff();
+
+#endif // RELAY_H
