bed29a6c60e73a9ece664c5201e5d0fea9c065b8
KLC81 <timlook81@gmail.com>
2026-02-18 23:25:56 +0800

    Add LCD control implementation

---
+++ Drivers/LCD/lcd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// LCD control implementation
+
+void lcd_init() {
+    // Code to initialize the LCD
+}
+
+void lcd_clear() {
+    // Code to clear the LCD screen
+}
+
+void lcd_print(const char* message) {
+    // Code to print a message on the LCD
+}
+
+void lcd_set_cursor(int row, int col) {
+    // Code to set the cursor position on the LCD
+}
