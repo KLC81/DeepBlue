1cf32fac87dae779e04c94088220890dd0ef6a78
KLC81 <timlook81@gmail.com>
2026-02-18 23:23:40 +0800

    Add device configuration header file

---
+++ Config/device_config.h
@@ -0,0 +1,13 @@
+// Device Configuration Header File
+
+#ifndef DEVICE_CONFIG_H
+#define DEVICE_CONFIG_H
+
+// Define device parameters
+#define DEVICE_NAME "DeepBlue"
+#define DEVICE_VERSION "1.0"
+#define DEVICE_ID 001
+
+// Add additional configuration options as needed
+
+#endif // DEVICE_CONFIG_H
