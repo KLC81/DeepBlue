89cf853872ffca332cee2d54a4a4a0cbb0086082
KLC81 <timlook81@gmail.com>
2026-02-18 23:26:24 +0800

    Create pin_config.h with pin mapping configuration

---
+++ Config/pin_config.h
@@ -0,0 +1,10 @@
+# Pin Mapping Configuration
+
+// Define pin mappings for various components
+
+#define PIN_LED 13 // LED pin
+#define PIN_BUTTON 2 // Button pin
+#define PIN_SENSOR 3 // Sensor pin
+#define PIN_MOTOR 9 // Motor pin
+
+// Add any additional pin mappings below
